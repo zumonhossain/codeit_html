d713dfb6d0e93ec1c36560d7eb7626eee3eb8500
Zumon Hossain <zumon15-13169@diu.edu.bd>
2024-08-26 19:30:04 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,140 @@
+$(document).ready(function ($) {
+    "use strict";
+
+
+
+    // owlCarousel
+    $('.slider_active').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            767: {
+                items: 1
+            },
+            992: {
+                items: 1
+            }
+        }
+    })
+
+
+
+    //magnificPopup
+    $('#youtube-video').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: 'http://www.youtube.com/embed/%id%?autoplay=1'
+                },
+            },
+
+            srcAction: 'iframe_src',
+        }
+    });
+
+
+    // counter_number
+    $('.counter_number').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+    // service_counter
+    $('.service_counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+
+    //Video Service magnificPopup
+    $('#video_service').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: 'http://www.youtube.com/embed/%id%?autoplay=1',
+                },
+            },
+
+            srcAction: 'iframe_src',
+        }
+    });
+
+    //faq_video magnificPopup
+    $('#faq_youtube_video').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: 'http://www.youtube.com/embed/%id%?autoplay=1',
+                },
+            },
+
+            srcAction: 'iframe_src',
+        }
+    });
+
+
+    // testimonial_active
+    $('.testimonial_client_active').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            767: {
+                items: 1
+            },
+            992: {
+                items: 1
+            }
+        }
+    })
+
+    $('.skillbar').skillBars({
+        // options here
+    });
+
+
+
+    $('.gallerys').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+}(jQuery));
